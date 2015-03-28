184b1c4a1bd2758ceac32b7bc302b4a942a850e1
LeeW <cl87654321@gmail.com>
2015-03-29 00:09:25 +0800

    Parse all grading data in both table and json format

---
+++ Ncku_grade_crawler.py
@@ -0,0 +1,102 @@
+import getpass
+from html.parser import HTMLParser
+
+import requests
+from HTML_Form_Parser.HTML_form_parser import HTMLFormParser
+
+
+class NckuGradeCrawler:
+    MAIN_URL = "http://140.116.165.71:8888/ncku/"
+    LOGIN_URL = MAIN_URL+"qrys02.asp"
+    LOGOUT_URL = MAIN_URL+"logouts.asp"
+    INDEX_URL = MAIN_URL+"/qrys05.asp"
+    ENCODING = "big5"
+    header = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+              'X-Requested-With': 'XMLHttpRequest'}
+
+    def __init__(self):
+        self.s = requests.session()
+
+    def login(self):
+        self.s.post(NckuGradeCrawler.LOGIN_URL, data=self.data)
+
+    def set_stu_info(self, stu_id, passwd):
+        self.data = {'ID': stu_id.upper(), 'PWD': passwd}
+
+    def get_available_semester_name(self):
+        req = self.s.post(NckuGradeCrawler.INDEX_URL,
+                          data=self.data,
+                          cookies=self.s.cookies)
+        # req.encoding = NckuGradeCrawler.ENCODING
+
+        p = SemeseterNameParser()
+        for line in req.text.splitlines():
+            p.feed(line)
+        return p.get_semesters()
+
+    def get_all_semester_data(self, json=False):
+        self.all_semester = dict()
+        sems = self.get_available_semester_name()
+        for s in sems:
+            ss = s[:4] + ("上" if "¤U" in s else "下")
+            self.all_semester[ss] = self.get_semeseter_data(s, json)
+        return self.all_semester
+
+    def get_semeseter_data(self, semeseter_name, json=False):
+        param = {'submit1': bytes(semeseter_name, 'cp1252')}
+        req = self.s.post(NckuGradeCrawler.INDEX_URL,
+                          params=param,
+                          data=self.data,
+                          headers=NckuGradeCrawler.header,
+                          cookies=self.s.cookies)
+        req.encoding = NckuGradeCrawler.ENCODING
+
+        p = HTMLFormParser()
+        for line in req.text.splitlines():
+            p.feed(line)
+        data = p.get_tables()[3][1:-2]
+
+        if json:
+            return self.__table_to_json(data)
+        else:
+            return data
+
+    def __table_to_json(self, table):
+        table_json = list()
+        for row in table[1:]:
+            json_element = dict()
+            for index, col in enumerate(row):
+                title = table[0][index]
+                json_element[title] = col
+            table_json.append(json_element)
+        return table_json
+
+    def logout(self):
+        self.s.post(NckuGradeCrawler.LOGOUT_URL)
+
+
+class SemeseterNameParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.semeseters = list()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "input":
+            self.semeseters.append(attrs[2][1])
+
+    def get_semesters(self):
+        return (self.semeseters)
+
+
+if __name__ == '__main__':
+    stu_id = input("Please input student ID: ")
+    passwd = getpass.getpass("Please input password: ")
+
+    g = NckuGradeCrawler()
+    g.set_stu_info(stu_id, passwd)
+    g.login()
+    data = g.get_all_semester_data(json=True)
+    for i in data:
+        print(i, data[i])
+
+    g.logout()
